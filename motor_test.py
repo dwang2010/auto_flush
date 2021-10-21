2cce1826b704f3c4ef24ca0508d59ad5e98cc690
darren wang <dachwadachwa@gmail.com>
2021-10-21 16:06:19 -0700

    add flush method based on fixed gear position

--- motor_test.py
+++ motor_test.py
@@ -2,6 +2,7 @@
 https://sourceforge.net/p/raspberry-gpio-python/wiki/PWM/
 
 configure raspberry pi pin for servo motor control
+- raspberry pi 3 model B+
 
 duty cycle (dc) takes value between (0-100)
 
@@ -15,18 +16,47 @@ supplied pulse train duty cycle
 import time
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD) # use header pin numbering
-GPIO.setup(16, GPIO.OUT) # configure pin 16 as output
+servo_ctrl = 12
 
-servo = GPIO.PWM(16, 50) # (channel, frequency)
-servo.start(0)
+GPIO.setmode(GPIO.BCM) # use logical pin names vs board pins
+GPIO.setup(servo_ctrl, GPIO.OUT) # configure servo_ctrl pin as output
 
-for i in range(5):
-    servo.ChangeDutyCycle(40)
+servo1 = GPIO.PWM(servo_ctrl, 50) # (channel, frequency)
+servo1.start(0)
+time.sleep(1)
+
+print ("starting sequence")
+
+def move_arm(val: int) -> None:
+    print ("current setting: {}".format(val))
+    servo1.ChangeDutyCycle(val)
+    time.sleep(0.5)
+
+    servo1.ChangeDutyCycle(0) # to reduce jitter
     time.sleep(1)
 
-    servo.ChangeDutyCycle(20)
+def flush() -> None:
+    # start upright at 90 degrees
+    move_arm(8)
+
+    # move down to 0 degrees
+    move_arm(4)
     time.sleep(1)
 
-servo.stop()
+    # return to idle position
+    move_arm(8)
+
+# range of motor limited between 2-12% duty cycle
+# slightly less than 270 degree travel
+
+#for i in range(2, 13):
+#    move_arm(i)
+#move_arm(2)
+#time.sleep(1)
+
+flush()
+
+print ("shutting down")
+servo1.stop()
 GPIO.cleanup()
+print ("exiting!")
